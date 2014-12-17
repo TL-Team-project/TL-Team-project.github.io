705e9a0cdd3fd0f15a1c9918212fea51b58f410f
marcopanino <marco@softml.it>
2014-12-17 23:19:21 +0100

    sostituito index.html con index.php e inclusi header e footer

---
+++ header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="it">
+    <head>
+        <meta charset="utf-8">
+        <!-- Viewport -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Titolo, descrizione, autore -->
+        <title>Regione Veneto</title>
+        <meta name="description" content="Informazioni sulla nostra regione Veneto">
+        <meta name="author" content="Studenti del corso di Web Design">
+        <!-- CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/stileSito.css" rel="stylesheet">
+    </head>
